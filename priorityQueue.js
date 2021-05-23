d563d6fd4f0befc08dfc09d624d2c1b9ec57b500
Paolo Speziali <speziali99@gmail.com>
2021-05-23 12:57:23 +0200

    commit pre-modifica

---
+++ priorityQueue.js
@@ -0,0 +1,13 @@
+// User defined class
+// to store element and its priority
+module.exports = class QueueElement {
+    constructor(v, fScore)
+}
+
+module.exports = class PriorityQueue {
+    constructor(){
+        this.list = new Array()
+    }
+
+    
+};
